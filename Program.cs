f8aaae2d8d0b8f103fec2aa7d97235597fca258c
rofustus <rtramon2@rockets.utoledo.edu>
2023-05-12 15:04:22 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,10 +2,14 @@ using Newtonsoft.Json;
 using System.Text;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
+using System.Runtime.CompilerServices;
+using System.Net;
+
 class Program
 {
     static async Task Main(string[] args)
     {
+        Console.Out.Flush();
         SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
         SpeechSynthesizer synth = new SpeechSynthesizer();
         // Configure the recognizer to use the default system microphone
@@ -18,8 +22,12 @@ class Program
         synth.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
         synth.Rate = 1;
         string prompt = "";
+        // Define an array of motivational messages to keep the user engaged
+       
+        Console.Out.Flush();
         while (true)
         {
+            Console.Out.Flush();
             Grammar grammar = new Grammar(grammarBuilder);
 
             // Load the grammar into the speech recognizer
@@ -27,18 +35,25 @@ class Program
             RecognitionResult result = recognizer.Recognize();
             prompt = result.Text;
             Console.WriteLine(prompt);
+            Console.Out.Flush();
+            Console.WriteLine("Waiting for response... ");
+            Console.Out.Flush();
             //Console.WriteLine("Hello, World!");
             //prompt = Console.ReadLine();
             var client = new HttpClient();
             var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:5000/api/v1/generate");
-            var content = new StringContent("{\r\n    \"prompt\": \"" + prompt + "\",\r\n    \"max_new_tokens\": 25,\r\n    \"do_sample\": true,\r\n    \"temperature\": 1.3,\r\n    \"top_p\": 0.1,\r\n    \"typical_p\": 1,\r\n    \"repetition_penalty\": 1.18,\r\n    \"top_k\": 40,\r\n    \"min_length\": 0,\r\n    \"no_repeat_ngram_size\": 0,\r\n    \"num_beams\": 1,\r\n    \"penalty_alpha\": 0,\r\n    \"length_penalty\": 1,\r\n    \"early_stopping\": false,\r\n    \"seed\": -1,\r\n    \"add_bos_token\": true,\r\n    \"truncation_length\": 2048,\r\n    \"ban_eos_token\": false,\r\n    \"skip_special_tokens\": true,\r\n    \"stopping_strings\": []\r\n}", Encoding.UTF8, "application/json");
+            
+            // Send the request and display the response
+            var content = new StringContent("{\r\n    \"prompt\": \"" + prompt + "\",\r\n    \"max_new_tokens\": 200,\r\n    \"do_sample\": true,\r\n    \"temperature\": 1.3,\r\n    \"top_p\": 0.1,\r\n    \"typical_p\": 1,\r\n    \"repetition_penalty\": 1.18,\r\n    \"top_k\": 40,\r\n    \"min_length\": 0,\r\n    \"no_repeat_ngram_size\": 0,\r\n    \"num_beams\": 1,\r\n    \"penalty_alpha\": 0,\r\n    \"length_penalty\": 1,\r\n    \"early_stopping\": false,\r\n    \"seed\": -1,\r\n    \"add_bos_token\": true,\r\n    \"truncation_length\": 2048,\r\n    \"ban_eos_token\": false,\r\n    \"skip_special_tokens\": true,\r\n    \"stopping_strings\": []\r\n}", Encoding.UTF8, "application/json");
             request.Content = content;
             var response = await client.SendAsync(request);
             response.EnsureSuccessStatusCode();
+         
             dynamic parsedJson = JsonConvert.DeserializeObject(await response.Content.ReadAsStringAsync());
             string text = parsedJson.results[0].text;
 
             Console.WriteLine(text);//await response.Content.ReadAsStringAsync());
+            Console.Out.Flush();
             synth.Speak(text);
             
 
